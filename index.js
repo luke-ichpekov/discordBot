3ede6e9a6aa4e0fc33d6db1a406c8f2163713507
luke <luke.ichpekov@gmail.com>
2020-06-19 15:10:14 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,90 @@
+const {
+    Client,
+    Attachment
+} = require('discord.js');
+const bot = new Client();
+
+const cheerio = require('cheerio');
+
+const request = require('request');
+
+
+
+const token = 'NzAxMTIzODAyOTIyNDgzNzgy.XuveBg.AyM0TyF5Aid_Lc4cxXMGND6RD7E';
+
+const PREFIX = '!';
+
+var version = '1.3';
+
+
+bot.on('ready', () => {
+    console.log('This bot is online! ' + version);
+
+})
+
+
+
+
+bot.on('message', message => {
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+        case 'image':
+        var secondWord = args[1];
+        image(message,secondWord);
+
+        break;
+    }
+
+});
+
+function image(message,secondWord){
+
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + secondWord,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+
+
+
+
+
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+  
+ 
+        $ = cheerio.load(responseBody); 
+ 
+
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+        
+        console.log(urls);
+
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+
+
+
+
+
+
+
+}
+
+bot.login(token);
